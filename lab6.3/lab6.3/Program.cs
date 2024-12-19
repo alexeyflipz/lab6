dae7d35e62a3ff4f2bcb7a27d23bc712120692f3
alexeyflipz <58476252+alexeyflipz@users.noreply.github.com>
2024-12-19 19:09:48 +0200

    Add files via upload

---
+++ lab6.3/lab6.3/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+public delegate void TaskExecution<TTask>(TTask task);
+
+delegate TPriority PrioritySelector<TTask, TPriority>(TTask task);
+
+public class TaskScheduler<TTask, TPriority> where TPriority : IComparable<TPriority>
+{
+    private SortedDictionary<TPriority, Queue<TTask>> _taskQueue;
+    private TaskExecution<TTask> _taskExecution;
+
+    public TaskScheduler(TaskExecution<TTask> taskExecution)
+    {
+        _taskQueue = new SortedDictionary<TPriority, Queue<TTask>>();
+        _taskExecution = taskExecution;
+    }
+
+    public void AddTask(TTask task, TPriority priority)
+    {
+        if (!_taskQueue.ContainsKey(priority))
+        {
+            _taskQueue[priority] = new Queue<TTask>();
+        }
+        _taskQueue[priority].Enqueue(task);
+    }
+
+    public void ExecuteNext()
+    {
+        if (_taskQueue.Count == 0)
+        {
+            Console.WriteLine("Немає завдань у черзі.");
+            return;
+        }
+
+        var highestPriority = GetHighestPriority();
+        var task = _taskQueue[highestPriority].Dequeue();
+
+        if (_taskQueue[highestPriority].Count == 0)
+        {
+            _taskQueue.Remove(highestPriority);
+        }
+
+        _taskExecution(task);
+    }
+
+    private TPriority GetHighestPriority()
+    {
+        foreach (var priority in _taskQueue.Keys)
+        {
+            return priority;
+        }
+        throw new InvalidOperationException("Черга пуста.");
+    }
+
+    public bool HasTasks() => _taskQueue.Count > 0;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var scheduler = new TaskScheduler<string, int>((task) =>
+        {
+            Console.WriteLine($"Виконання завдання: {task}");
+        });
+
+        while (true)
+        {
+            Console.WriteLine("1: Додати завдання\n2: Виконати наступне завдання\n3: Вийти");
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.Write("Введіть завдання: ");
+                    var task = Console.ReadLine();
+                    Console.Write("Введіть пріоритет (число): ");
+                    if (int.TryParse(Console.ReadLine(), out var priority))
+                    {
+                        scheduler.AddTask(task, priority);
+                        Console.WriteLine("Завдання додано.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Невірний пріоритет.");
+                    }
+                    break;
+                case "2":
+                    scheduler.ExecuteNext();
+                    break;
+                case "3":
+                    return;
+                default:
+                    Console.WriteLine("Невірний вибір.");
+                    break;
+            }
+        }
+    }
+}
---
+++ lab6.3/lab6.3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lab6.3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab6.3")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4fbbd6b5-ea80-4434-99bb-595a29ec986c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
